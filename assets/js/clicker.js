f3be5f948d2884ac4b29a04a10ef15cfebc1599f
John <john.laterre@gmail.com>
2020-07-20 15:26:17 +0200

    First working version (single multiplier)

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -2,7 +2,7 @@
 
 (() => {
 
-    let counter = localStorage.getItem("count");
+    let counter = Number(localStorage.getItem("count"));
     console.log(counter);
     if (counter === null || counter === "undefined") {
         counter = 0;
@@ -10,9 +10,8 @@
     document.getElementById("counter").innerHTML = counter;
 
     document.getElementById("clicker").addEventListener("click", () => {
-        counter++;
+        counter = counter + (Number(_multiplier));
         localStorage.setItem("count", counter);
-        console.log(counter);
         document.getElementById("counter").innerHTML = counter;
     });
 
@@ -21,5 +20,44 @@
         document.location.reload();
         document.getElementById("counter").innerHTML = counter;
     });
+
+    // John
+
+    const multiplier = document.getElementById("multiplier");
+    const clicker = document.getElementById("clicker");
+    const value = document.getElementById("counter");
+    const cost = document.getElementById("cost");
+    let _multiplier = 1;
+    let _cost = 10;
+    let sufficientFunds = false;
+
+    multiplier.addEventListener(
+        "click",
+        () => (updateUI(), false),
+    );
+
+    function updateUI() {
+        balanceCheck();
+        if (sufficientFunds) {
+            // Update variables
+            counter = counter - _cost;
+            _multiplier *= 2;
+            _cost = 10 * _multiplier;
+            // Update UI
+            value.innerHTML = counter;
+            clicker.innerHTML = `Click Me x${_multiplier}`;
+            cost.innerHTML = `${_cost}$`;
+            // Reset balance
+            sufficientFunds = false;
+        }
+    }
+
+    function balanceCheck() {
+        if (counter >= _cost) {
+            sufficientFunds = true;
+        } else {
+            alert(`Insufficient funds!`);
+        }
+    }
 
 })();
--- assets/js/multiplier.js
+++ assets/js/multiplier.js
@@ -2,8 +2,8 @@ const multiButton = document.getElementById("multi");
 const clicker = document.getElementById("clicker");
 const counter = document.getElementById("counter");
 const cost = document.getElementById("cost");
-const priceMultiplier = 1;
-const enoughFunds = false;
+let priceMultiplier = 1;
+let enoughFunds = false;
 
 multiButton.addEventListener(
     "click",
@@ -12,15 +12,18 @@ multiButton.addEventListener(
 
 function updateUI() {
     balanceCheck();
+    console.log(enoughFunds);
     if (enoughFunds) {
-        clicker.innerHTML = Number(multiButton.value);
-        priceMultiplier *= 1.5;
+        clicker.innerHTML = `${clicker.innerHTML} x2`;
 
+        priceMultiplier *= 2;
+        counter.innerHTML = counter.innerHTML - cost.innerHTML;
+        cost.innerHTML = cost.innerHTML * priceMultiplier;
     }
 }
 
 function balanceCheck() {
-    if (Number(counter.value) >= Number(cost.value)) {
+    if (Number(counter.innerHTML) >= Number(cost.innerHTML)) {
         enoughFunds = true;
     }
 }
