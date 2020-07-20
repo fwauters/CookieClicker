28623d97fa1ccb4a957e103a2d509f047d5b0f24
François Sin <fwauters13@gmail.com>
2020-07-20 14:21:46 +0200

    update autoclick

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -22,4 +22,21 @@
         document.getElementById("counter").innerHTML = counter;
     });
 
+    document.getElementById("autoclicker").addEventListener("click", () => {
+        AutoClicker();
+    });
+
+    let priceAC = 10; // Valeur temporaire du prix
+    function autoAdd() {
+        counter++; // +1
+        document.getElementById("counter").innerHTML = counter; // Afficher
+    }
+    function AutoClicker() { // Fonction autoclick
+        if (counter>=10) {   // Pas de négatif
+            counter -= priceAC; // Retirer les points               
+            autoAdd(); // Appel de la fonction qui fait +1
+            setInterval (autoAdd, 1000); // Intervalle d'une seconde
+        }
+    }
+
 })();
