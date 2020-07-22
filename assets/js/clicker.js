1a48bd96abfcdcdb0761548216e66e2ffb116ec8
François Sin <fwauters13@gmail.com>
2020-07-22 11:57:36 +0200

    update

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -1,5 +1,3 @@
-
-
 (() => {
 
     // Base pour counter + boutons click et reset
@@ -10,31 +8,43 @@
         counter = 0;
     }
     document.getElementById("counter").innerHTML = counter;
+    counter = Number(counter);
 
     let _multiplier = localStorage.getItem("multiplier");
     console.log(_multiplier);
     if (_multiplier === null || _multiplier === "undefined") {
         _multiplier = 1;
     }
+    _multiplier = Number(_multiplier);
+    document.getElementById("clicker").innerHTML = "Click Me " + "(+" + _multiplier + ")";
+    
+    //bonus flag
+    let bonusBoolean = false;
 
     document.getElementById("clicker").addEventListener("click", () => {
-        counter++;
+        //if bonus activated then add twice the multiplier
+        if (bonusBoolean){
+            counter = counter + _multiplier *2;
+        } else {
+            counter = counter + _multiplier;
+        }
+
         localStorage.setItem("count", counter);
         console.log(counter);
         document.getElementById("counter").innerHTML = counter;
 
         // Tests pour activer les autres boutons
-        if (counter >= 50) {
-            document.getElementById("x2").disabled = false;
+        if (counter >= parseInt(document.getElementById("2").textContent)) {
+            document.getElementById("2").disabled = false;
         }
-        if (counter >= 200) {
-            document.getElementById("x3").disabled = false;
+        if (counter >= parseInt(document.getElementById("3").textContent)) {
+            document.getElementById("3").disabled = false;
         }
-        if (counter >= 500) {
-            document.getElementById("x4").disabled = false;
+        if (counter >= parseInt(document.getElementById("4").textContent)) {
+            document.getElementById("4").disabled = false;
         }
-        if (counter >= 2000) {
-            document.getElementById("x5").disabled = false;
+        if (counter >= parseInt(document.getElementById("5").textContent)) {
+            document.getElementById("5").disabled = false;
         }
         if (counter >= 5000) {
             document.getElementById("autoclicker").disabled = false;
@@ -50,16 +60,15 @@
         document.getElementById("counter").innerHTML = counter;
 
         // Reset des boutons de droite
-        document.getElementById("x2").disabled = true;
-        document.getElementById("x3").disabled = true;
-        document.getElementById("x4").disabled = true;
-        document.getElementById("x5").disabled = true;
+        document.getElementById("2").disabled = true;
+        document.getElementById("3").disabled = true;
+        document.getElementById("4").disabled = true;
+        document.getElementById("5").disabled = true;
         document.getElementById("autoclicker").disabled = true;
         document.getElementById("bonus").disabled = true;
     });
 
     // Boutons multiplicateurs et bonus
-    
     Array.from(document.querySelectorAll("button.multiplier")).forEach($btn =>
         $btn.addEventListener(
             "click",
@@ -67,80 +76,78 @@
         )
     );
 
-
     document.getElementById("autoclicker").addEventListener("click", () => {
         AutoClicker();
     });
 
     document.getElementById("bonus").addEventListener("click", () => {
-         //gets the current counter
-    counter = parseInt(localStorage.getItem("count"));
-    let price = 100;
-     
-    //let counter = parseInt(document.getElementById("counter").innerHTML);
-    //if the counter > the required price
-    if (counter > price){
-        //reduces the counter
-        counter = counter - price;  
-        //saves the new value of counter
-        localStorage.setItem("count", counter);
-        //displays the new value of the counter
-        //document.getElementById("counter").innerHTML = counter.toString(10);
-        document.getElementById("counter").innerHTML = localStorage.getItem("count");
-        
-        //sets the Bonus active
-        bonusBoolean = true;
+        //gets the current counter
+        counter = parseInt(localStorage.getItem("count"));
+        let bonusPrice = parseInt(document.getElementById("bonus").innerHTML,10);
 
-        //sets the bonus button disabled
-        document.getElementById("bonus").disabled=true;
-        //sets the timer at start time
-        let time = 10; 
-        //launches the count down on the timer
-        var x = setInterval(function() {
-            let string;
-            //sets the click button with the new display
-            document.getElementById("clicker").innerHTML = `Bonus 200% for ${time}s`;
+        //let counter = parseInt(document.getElementById("counter").innerHTML);
+        //if the counter > the required price
+        if (counter > bonusPrice) {
+            //reduces the counter
+            counter = counter - bonusPrice;
+            //saves the new value of counter
+            localStorage.setItem("count", counter);
+            //displays the new value of the counter
+            document.getElementById("counter").innerHTML = counter;
             
-            //decreases the time
-            time = time - 1;
-            
-            // if count down completed
-            if (time < 0){
-                // stop the timer
-                clearInterval(x);
+            //saves the display of the button click-me , before activating the bonus
+            let clickMePreviousDisplayString = document.getElementById("clicker").innerHTML;
 
-                //sets back the display of the click button
-                //console.log("end timer : multiplier: " + multiplier);
-                switch(multiplier){
-                    case 1 : string = "+1"; break;
-                    case 2 : string = "+2"; break;
-                    case 3 : string = "+3"; break;
-                    case 4 : string = "+4"; break;
-                    case 5 : string = "+5"; break;
+            //sets the bonus button disabled
+            document.getElementById("bonus").disabled = true;
+
+            //activates the bonus flag 
+            bonusBoolean = true;
+
+            //sets the timer at start time
+            let time = 10;
+            //launches the count down on the timer
+            var x = setInterval(function () {
+                //sets the click button with the new display
+                document.getElementById("clicker").innerHTML = `Bonus 200% for ${time}s (+${_multiplier*2})`;
+
+                //decreases the timer
+                time = time - 1;
+
+                // if count down completed
+                if (time < 0) {
+                    // stop the timer
+                    clearInterval(x);
+
+                    //deactivates the bonus flag 
+                    bonusBoolean = false;
+
+                    //displays in the button clicker, the previous counter display
+                    document.getElementById("clicker").innerHTML = clickMePreviousDisplayString;
+                    //sets the bonus button enabled
+                    document.getElementById("bonus").disabled = false;
                 }
-
-                //sets inactive the bonus option 
-                bonusBoolean = false;
-                //displays in the button clicker, the previous counter
-                document.getElementById("clicker").innerHTML = string;
-                //sets the bonus button enabled
-                document.getElementById("bonus").disabled=false;
-            } 
-        },1000);}
+            }, 1000);
+        }
     });
 
-
     // Fonctions
-    let priceAC = 10; // Valeur temporaire du prix
+    let priceAC = 5000; // Valeur temporaire du prix
     function autoAdd() {
-        counter++; // +1
+        //counter++; // +1
+        //if bonus activated then add twice the multiplier
+        if (bonusBoolean){
+            counter = counter + _multiplier *2;
+        } else {
+            counter = counter + _multiplier;
+        }
         document.getElementById("counter").innerHTML = counter; // Afficher
     }
     function AutoClicker() { // Fonction autoclick
-        if (counter>=10) {   // Pas de négatif
+        if (counter >= priceAC) {   // Pas de négatif
             counter -= priceAC; // Retirer les points               
             autoAdd(); // Appel de la fonction qui fait +1
-            setInterval (autoAdd, 1000); // Intervalle d'une seconde
+            setInterval(autoAdd, 1000); // Intervalle d'une seconde
         }
     }
 
@@ -150,19 +157,20 @@
         const clicker = document.getElementById("clicker");
         const inflation = 2;
 
-        _cost = parseInt((multiplier.innerHTML), 10);
+        let cost = parseInt((multiplier.innerHTML), 10);
 
-        
-            _multiplier = parseInt(multiplier.value, 10);
+        _multiplier = _multiplier * Number(id);
 
-            // Update variables
-            counter = counter - _cost;
-            _cost = _cost * inflation;
+        // Update variables
+        counter = counter - cost;
+        cost = cost * inflation;
 
-            // Update UI
-            score.innerHTML = counter;
-            clicker.innerHTML = `Click Me (x${_multiplier})`;
-            multiplier.innerHTML = _cost;      
+        // Update UI
+        score.innerHTML = counter;
+        clicker.innerHTML = `Click Me (+${_multiplier})`;
+        multiplier.innerHTML = `${cost} $`;
+        document.getElementById(id).disabled = true;
+        localStorage.setItem("multiplier", _multiplier);
     }
 
 })();
